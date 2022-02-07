95b0e664601ed86629fdbcd93be14c19ced0af72
WangJie <2740469261@qq.com>
2022-02-07 15:49:30 +0800

    refactor: use python md5 lib instead of js
    md.js is no longer needed

--- getToken.py
+++ getToken.py
@@ -1,9 +1,6 @@
 # -*-coding:utf-8-*-
-import execjs
-import os
-def get_token(port_name, port_number):
-	os.chdir(os.path.dirname(__file__))
-    with open('md5.js', 'r') as f:
-        t = f.read()
-    desJS = execjs.compile(t)
-    return desJS.call('md5', port_name + port_number + 'ecnu1024')
+import hashlib
+
+def get_token(port_name: str, port_number: str):
+    string = port_name + port_number + 'ecnu1024'
+    return hashlib.md5(string.encode('utf-8')).hexdigest()
